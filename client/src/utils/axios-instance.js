525ffaba63baeaf77c6e7151858115a9b3020f28
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2023-07-07 10:47:07 +0500

    axios change

--- client/src/utils/axios-instance.js
+++ client/src/utils/axios-instance.js
@@ -1,7 +1,11 @@
 import axios from "axios";
+console.log(window.location.protocol);
+
+// localhost
+console.log(window.location.hostname);
 const axiosInstance=axios.create({
   // baseURL: "http://localhost:8080/api/v1"
-  baseURL: "/api/v1"
+  baseURL:"https://student-courses-full-stack-app.vercel.app/api/v1"
 });
 // https://student-course-t530.onrender.com
 axiosInstance.interceptors.request.use((config)=>{
