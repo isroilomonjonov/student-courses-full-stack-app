b33bf38ce05b922bd8b6d356d79a9692e19d30b9
Isroil Omonjonov <isroiljonomonjonov@gmail.com>
2023-07-13 16:34:06 +0500

    main change

--- client/src/utils/axios-instance.js
+++ client/src/utils/axios-instance.js
@@ -1,13 +1,13 @@
 import axios from "axios";
-const axiosInstance=axios.create({
-  baseURL: "http://localhost:8080/api/v1"
+const axiosInstance = axios.create({
+  baseURL: "/",
   // baseURL:"https://student-courses.onrender.com/api/v1"
 });
-axiosInstance.interceptors.request.use((config)=>{
-const token = localStorage.getItem("token");
-if(token){
-  config.headers["Authorization"]="Bearer " + token;
-}
-return config;
-})
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["Authorization"] = "Bearer " + token;
+  }
+  return config;
+});
 export default axiosInstance;
